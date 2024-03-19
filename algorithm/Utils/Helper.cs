f2f0fb5f58ae685dd313b1b63a5d7306a9433f9f
Amin Bayatpour <aiba@drivebox.at>
2024-03-19 15:31:03 +0100

    updated

---
+++ algorithm/Utils/Helper.cs
@@ -0,0 +1,20 @@
+﻿using algorithm.Data;
+
+namespace algorithm.Utils
+{
+    public class Helper
+    {
+         public int ConvertTimeToLegNumber(DateTime time)
+        {
+            TimeSpan duration = time - Globals.Form.StartTime;
+
+            //Calculate the total number of minutes in a TimeSpan
+            double totalMinutes = duration.TotalMinutes;
+
+            // Divide by 15 to find how many 15-minute intervals are contained in the duration
+
+            return (int)Math.Floor(totalMinutes / Globals.Form.LegDuration);
+        }
+
+    }
+}
